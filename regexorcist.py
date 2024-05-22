de3ef51c70a48d041f31f9df69f6c92c608ca876
Yasin Gunaydin <81875281+yasingunaydiin@users.noreply.github.com>
2024-05-22 21:54:54 +0300

    ✨ Update Version 0.0.3
     Version 0.0.3:
     > Added an image on top and text saying the logo and version.
     > Make window non resizable and remove maximize button.
     > Removed the name on top of gui.
     > Added settings and put settings in there.
     > Added message that says “Fill output” and "Fill input"
     > Added icons for settings and process

--- regexorcist.py
+++ regexorcist.py
@@ -1,8 +1,8 @@
 import re
 import tkinter as tk
 from tkinter import messagebox, filedialog
-import customtkinter as ctk # https://customtkinter.tomschimansky.com/documentation/color
-
+import customtkinter as ctk
+from PIL import Image, ImageTk
 
 
 # Default file paths and patterns
@@ -11,26 +11,22 @@ output_file = ""
 words_to_remove = ["__label__sports"]
 regex_to_remove = [r'[^\w\süğıöçŞİĞÜÇÖ\d\s]+']
 
-
-
-def toggle_regex():
-    global use_regex
-    use_regex = not use_regex
-
-
+# State variables for checkboxes
 remove_numbers = False
-remove_non_letters = False 
+remove_non_letters = False
+add_commas = False
 
-# Function to toggle the state of remove_numbers
 def toggle_remove_numbers():
     global remove_numbers
     remove_numbers = not remove_numbers
 
-# Function to toggle the state of remove_non_letters
 def toggle_remove_non_letters():
     global remove_non_letters
     remove_non_letters = not remove_non_letters
 
+def toggle_add_commas():
+    global add_commas
+    add_commas = not add_commas
 
 # Function to process text
 def process_text():
@@ -38,11 +34,16 @@ def process_text():
     
     input_file = input_file_entry.get()
     output_file = output_file_entry.get()
+
+    if not input_file:
+        messagebox.showwarning("Fill input", "Please specify an input file.")
+        return
+    elif not output_file:
+        messagebox.showwarning("Fill output", "Please specify an output file.")
+        return
     
     with open(input_file, 'r', encoding='utf-8') as file:
         text = file.read()
-
-    # text = text.replace('*', '* ')
 
     for word in words_to_remove:
         text = text.replace(word, '')
@@ -52,33 +53,15 @@ def process_text():
         text = re.sub(r'\d', '', text)  # Remove numbers if checkbox is checked
     if remove_non_letters:
         text = re.sub(r'[^a-zA-Z\s]', '', text)  # Remove non-letter characters if checkbox is checked
+    if add_commas:
+        words = text.split()
+        text = '\n'.join([f"{word}," for word in words])
 
     with open(output_file, 'w', encoding='utf-8') as file:
         file.write(text)
 
     word_count = len(text.split())
     messagebox.showinfo("Task Complete", f"The process is complete.\nWord count: {word_count}")
-
-
-# GUI
-window = ctk.CTk(fg_color=('#fff', '#222323'))
-window.title('RegExorcist')
-
-
-
-# Calculate the screen width and height
-screen_width = window.winfo_screenwidth()
-screen_height = window.winfo_screenheight()
-
-# Calculate the x and y coordinates for the center of the screen
-x_coordinate = (screen_width - 400) // 2  # Assuming width of 500 for the root window
-y_coordinate = (screen_height - 500) // 2  # Assuming height of 600 for the root window
-
-# Set the position of the Tk root window to the center
-window.geometry(f'400x500+{x_coordinate}+{y_coordinate}')
-
-# Set window size
-window.geometry("400x500")
 
 # Add word to remove list
 def add_word_to_remove():
@@ -101,131 +84,241 @@ def select_output_file():
     output_file_entry.delete(0, tk.END)
     output_file_entry.insert(0, output_file)
 
+# Function to open settings window
+def open_settings():
+    settings_root = tk.Toplevel(root)
+    settings_root.title("Settings")
 
+    # this removes the maximize button
+    settings_root.resizable(0,0)
+
+    # Calculate the screen width and height
+    screen_width = settings_root.winfo_screenwidth()
+    screen_height = settings_root.winfo_screenheight()
+
+    # Calculate the x and y coordinates for the center of the screen
+    x_coordinate = (screen_width - 400) // 2
+    y_coordinate = (screen_height - 100) // 2
+
+    # Set the position of the Tk root window to the center
+    settings_root.geometry(f'400x100+{x_coordinate}+{y_coordinate}')
+    
+    # Checkbox for removing numbers
+    remove_numbers_checkbox_var = tk.BooleanVar()
+    remove_numbers_checkbox = ctk.CTkCheckBox(
+        settings_root,
+        text="0-9",
+        variable=remove_numbers_checkbox_var,
+        command=toggle_remove_numbers,
+        text_color=('#252525', '#f4f4f4'),
+        border_width=2,
+        border_color='#e6e6e6',
+        checkbox_height=20,
+        checkbox_width=20,
+        hover='#fff'
+    )
+    remove_numbers_checkbox.grid(row=0, column=0, padx=10, pady=5)
+
+    # Checkbox for removing non-letter characters
+    remove_non_letters_checkbox_var = tk.BooleanVar()
+    remove_non_letters_checkbox = ctk.CTkCheckBox(
+        settings_root,
+        text="Non-Letters",
+        variable=remove_non_letters_checkbox_var,
+        command=toggle_remove_non_letters,
+        text_color=('#252525', '#f4f4f4'),
+        border_width=2,
+        border_color='#e6e6e6',
+        checkbox_height=20,
+        checkbox_width=20,
+        hover='#fff'
+    )
+    remove_non_letters_checkbox.grid(row=0, column=1, padx=20, pady=5)
+
+    # Checkbox for adding commas and new lines
+    add_commas_checkbox_var = tk.BooleanVar()
+    add_commas_checkbox = ctk.CTkCheckBox(
+        settings_root,
+        text="CSV",
+        variable=add_commas_checkbox_var,
+        command=toggle_add_commas,
+        text_color=('#252525', '#f4f4f4'),
+        border_width=2,
+        border_color='#e6e6e6',
+        checkbox_height=20,
+        checkbox_width=20,
+        hover='#fff'
+    )
+    add_commas_checkbox.grid(row=0, column=2, padx=10, pady=5)
+
+# GUI
+root = ctk.CTk()
+root.title('')
+root.configure(fg_color="#fff")
+
+# this removes the maximize button
+root.resizable(0,0)
+
+# Calculate the screen width and height
+screen_width = root.winfo_screenwidth()
+screen_height = root.winfo_screenheight()
+
+# Calculate the x and y coordinates for the center of the screen
+x_coordinate = (screen_width - 400) // 2
+y_coordinate = (screen_height - 450) // 2
+
+# Set the position of the Tk root window to the center
+root.geometry(f'400x450+{x_coordinate}+{y_coordinate}')
+
+
+
+# Load image and create PhotoImage object
+logo_image = Image.open("/Users/yasingunaydin/Documents/GitHub/RegExorcist/images/RegExorcist_mac.png")
+settings_image = ctk.CTkImage(Image.open("/Users/yasingunaydin/Documents/GitHub/RegExorcist/images/settings.png").resize((20,20)))
+process_image = ctk.CTkImage(Image.open("/Users/yasingunaydin/Documents/GitHub/RegExorcist/images/run.png").resize((20,20)))
+
+# Resize image
+image_width = 100  # Set your desired width
+image_height = 100  # Set your desired height
+myImage = logo_image.resize((image_width, image_height))
+
+photo = ImageTk.PhotoImage(myImage)
+
+# Get the background color of the window for logo
+window_bg_color = root.cget('bg')
+
+
+
+# Display image without border and with matching border color
+image_label = tk.Label(root, image=photo, bg=window_bg_color)
+image_label.grid(row=1, column=0, columnspan=3, padx=10, pady=0)
+
+# Add label for "RegExorcist" text
+regex_label = ctk.CTkLabel(
+    root,
+    text="RegExorcist",
+    text_color=('#252525', '#f4f4f4'),
+    font=("Helvetica", 25, "bold")
+)
+regex_label.grid(row=2, column=0, columnspan=3, padx=10, pady=0)
+
+# Add label for "version" text
+version_label = ctk.CTkLabel(
+    root,
+    text="Version 0.0.3",
+    text_color=('#252525', '#f4f4f4'),
+    font=("Helvetica", 10)
+)
+version_label.grid(row=3, column=0, columnspan=3, padx=10, pady=0)
 
 # Input File Entry Widget
 input_file_label = ctk.CTkLabel(
-    window,
+    root,
     text="Input File:",
     text_color=('#252525', '#f4f4f4')
-    )
-input_file_label.grid(row=0, column=0, padx=10, pady=5)
+)
+input_file_label.grid(row=4, column=0, padx=5, pady=5)
 
 input_file_entry = ctk.CTkEntry(
-    window,
-    width=150,
+    root,
+    width=200,
     fg_color='#fff',
     border_color='#e6e6e6'
-    )
-input_file_entry.grid(row=0, column=1, padx=10, pady=5)
+)
+input_file_entry.grid(row=4, column=1, padx=10, pady=5)
 
 input_file_button = ctk.CTkButton(
-    window,
+    root,
     text="Browse",
     command=select_input_file,
     fg_color=('#e6e6e6', '#3e3f3f'),
     text_color=('#252525', '#f4f4f4'),
     width=50,
     hover='#000'
-    )
-input_file_button.grid(row=0, column=2, padx=10, pady=5)
+)
+input_file_button.grid(row=4, column=2, padx=10, pady=5)
 
 # Output File Entry Widget
 output_file_label = ctk.CTkLabel(
-    window,
+    root,
     text="Output File:",
     text_color=('#252525', '#f4f4f4')
-    )
-output_file_label.grid(row=1, column=0, padx=10, pady=5)
+)
+output_file_label.grid(row=5, column=0, padx=10, pady=5)
 
 output_file_entry = ctk.CTkEntry(
-    window,
-    width=150,
+    root,
+    width=200,
     fg_color='#fff',
     border_color='#e6e6e6'
-    )
-output_file_entry.grid(row=1, column=1, padx=10, pady=5)
+)
+output_file_entry.grid(row=5, column=1, padx=10, pady=5)
 
 output_file_button = ctk.CTkButton(
-    window,
+    root,
     text="Browse",
     command=select_output_file,
     fg_color=('#e6e6e6', '#3e3f3f'),
     text_color=('#252525', '#f4f4f4'),
     width=50,
     hover='#000'
-    )
-output_file_button.grid(row=1, column=2, padx=10, pady=5)
+)
+output_file_button.grid(row=5, column=2, padx=10, pady=5)
 
 # Text Entry Widget
 label = ctk.CTkLabel(
-    window,
+    root,
     text="Specific:",
     text_color=('#252525', '#f4f4f4')
-    )
-label.grid(row=2, column=0, padx=10, pady=5)
+)
+label.grid(row=6, column=0, padx=10, pady=5)
 
 entry = ctk.CTkEntry(
-window,
-width=150,
-fg_color='#fff',
-border_color='#e6e6e6'
+    root,
+    width=200,
+    fg_color='#fff',
+    border_color='#e6e6e6'
 )
-entry.grid(row=2, column=1, padx=10, pady=5)
+entry.grid(row=6, column=1, padx=10, pady=5)
 
 # Button to Add Word to Remove List
 add_button = ctk.CTkButton(
-    window,
+    root,
     text="Add",
     command=add_word_to_remove,
     fg_color=('#e6e6e6', '#3e3f3f'),
     text_color=('#252525', '#f4f4f4'),
     width=50,
     hover='#000'
-    )
-add_button.grid(row=2, column=2, padx=10, pady=5)
+)
+add_button.grid(row=6, column=2, padx=10, pady=5)
 
-# Button to Process Text
+
+
+# Button to Process Text at the bottom
 process_button = ctk.CTkButton(
-    window,
-    text="Process Text",
+    root,
+    image=process_image,
     command=process_text,
+    text = '',
+    width=20,
+    height=35,
     fg_color=('#e6e6e6', '#3e3f3f'),
-    text_color=('#252525', '#f4f4f4'),
     hover='#000'
 )
-process_button.grid(row=4, column=1, padx=10, pady=5)
+process_button.grid(row=9, column=1,columnspan=2, pady=10)
 
-# Checkbox for removing numbers
-remove_numbers_checkbox_var = tk.BooleanVar()
-remove_numbers_checkbox = ctk.CTkCheckBox(
-    window, 
-    text="0-9", 
-    variable=remove_numbers_checkbox_var, 
-    command=toggle_remove_numbers,
-    text_color=('#252525', '#f4f4f4'),
-    border_width=2,
-    border_color='#e6e6e6',
-    checkbox_height=20,
-    checkbox_width=20,
-    hover='#fff'
+# Add settings button
+settings_button = ctk.CTkButton(
+    root,
+    image=settings_image,
+    command=open_settings,
+    text = '',
+    width=20,
+    height=35,
+    fg_color=('#e6e6e6', '#3e3f3f'),
+    hover='#000'
 )
-remove_numbers_checkbox.grid(row=3, column=0, padx=10, pady=5)
+settings_button.grid(row=9, column=0, columnspan=2, pady=10)
 
-# Checkbox for removing non-letter characters
-remove_non_letters_checkbox_var = tk.BooleanVar()
-remove_non_letters_checkbox = ctk.CTkCheckBox(
-    window, 
-    text="Non-Letters", 
-    variable=remove_non_letters_checkbox_var, 
-    command=toggle_remove_non_letters,
-    text_color=('#252525', '#f4f4f4'),
-    border_width=2,
-    border_color='#e6e6e6',
-    checkbox_height=20,
-    checkbox_width=20,
-    hover='#fff'
-)
-remove_non_letters_checkbox.grid(row=3, column=1, padx=20, pady=5)
-
-window.mainloop()
+root.mainloop()
